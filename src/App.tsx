fa6c007ee4230211f0824005db46ee70081bd647
kjohndcb <>
2021-05-18 20:03:34 -0700

    refactor: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+  useLocation,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
 import Header from '@/components/Header';
 import GlobalStyle from '@/styles/global';
 import theme from '@/styles/theme';
 import routes from '@/routes.json';
+import store from '@/store';
+
+const generatedRoutes = generateRoutes();
 
 interface RouteInterface {
   path: string;
@@ -14,20 +23,25 @@ interface RouteInterface {
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Router>
-          <Route
-            render={({ location }) => (
-              <>
-                <Header />
-                <Switch location={location}>{generateRoutes()}</Switch>
-              </>
-            )}
-          />
+          <Content />
         </Router>
       </ThemeProvider>
+    </Provider>
+  );
+}
+
+function Content() {
+  const location = useLocation();
+  return (
+    <>
+      <Header />
+      <Switch location={location} key={location.key}>
+        {generatedRoutes}
+      </Switch>
     </>
   );
 }
