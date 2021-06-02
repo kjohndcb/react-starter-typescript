eba060f616fbc90d0eb89f2f371fb366aa34be9b
kjohndcb <>
2021-06-01 20:11:37 -0700

    feat(theme): add breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,5 @@
+export const breakpoints: number[] = [576, 768, 992, 1140, 1600];
+
 const theme = {
   colors: {
     background: '#fff',
@@ -12,8 +14,16 @@ const theme = {
   zIndex: {
     header: 1000,
   },
+  media: {
+    sm: `${breakpoints[0]}px`,
+    md: `${breakpoints[1]}px`,
+    lg: `${breakpoints[2]}px`,
+    xl: `${breakpoints[3]}px`,
+    xxl: `${breakpoints[4]}px`,
+  },
   // styled-system properties
   space: [],
+  breakpoints: breakpoints.map((breakpoint) => `${breakpoint}px`),
 };
 
 export default theme;
